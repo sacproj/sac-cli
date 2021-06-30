89549dd520861adccd1b06c901f4b9603af1ab42
Stéphane Este-Gracias <sestegra@gmail.com>
2021-06-30 17:03:45 +0200

    fix: build slides deck in PDF timeout
    Signed-off-by: Stéphane Este-Gracias <sestegra@gmail.com>

--- pdf.js
+++ pdf.js
@@ -4,12 +4,12 @@ const puppeteer = require('puppeteer');
   const url = process.argv[2];
   const output = process.argv[3];
   const timeout = process.argv[4];
-  console.log(`Processing ${url} to ${output}`);
+  console.log(`Processing ${url} to ${output} with timeout set to ${timeout} ms`);
   // '--no-sandbox' is set to allow running on Docker without '--cap-add SYS_ADMIN'
   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage();
-  await page.goto(url, { waitUntil: 'networkidle0' });
-  await page.waitForSelector('.print-pdf', {visible: true, timeout: timeout })
+  await page.goto(url, { waitUntil: 'networkidle0', timeout: timeout });
+  await page.waitForSelector('.print-pdf', {visible: true, timeout: timeout });
   await page.pdf({ path: output, format: 'A4' });
   await browser.close();
 })();
